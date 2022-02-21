f09e6e63cc96db8950c4c77d7b1849873ee9eec6
PavelPospolit <pavel.pospolit@telekom.de>
2022-02-21 12:02:35 +0100

    filtering

--- my-app/src/components/ReservationPage.js
+++ my-app/src/components/ReservationPage.js
@@ -2,12 +2,17 @@ import React, { useEffect, useState } from 'react'
 import HomePage from './HomePage'
 import DateTimePicker from 'react-datetime-picker'
 import { format } from "date-fns"
+import Select from 'react-select'
+import 'react-dropdown/style.css'
 
 function ReservationPage({ email, reservationPage, setReservationPage,
     employee, setEmployee, rooms, setRoom, homePage, setHomePage, loggedInEmployeeID,
     startTime, setStartTime, formattedStartTime, setFormattedStartTime, endTime, setEndTime, formattedEndTime, setFormattedEndTime, reservations, setReservations }) {
 
     const [roomnumber, setRoomnumber] = useState('')
+    const [filterOption, setFilterOption] = useState('')
+    const [filter, setFilter] = useState('')
+    const [filteredList, setfilteredList] = useState()
 
     useEffect(() => {
         fetch('/rooms')
@@ -16,13 +21,28 @@ function ReservationPage({ email, reservationPage, setReservationPage,
     }, [setRoom])
 
 
-    const roomlist = rooms.map((room) =>
-        <tr key={room.Roomnumber} id={room.Roomnumber} onClick={() => { setRoomnumber(room.Roomnumber); let allElements = document.getElementsByClassName('selected'); for (let i = 0; i < allElements.length; i++) { allElements[i].classList.remove('selected'); }; document.getElementById(`${room.Roomnumber}`).classList.add('selected') }} style={{ cursor: 'pointer' }}>
-            <td>{room.Roomnumber}</td>
-            <td>{room.Roomdescritpion}</td>
-            <td>{room.Roomspecials}</td>
-            <td>{room.Roomcapacity}</td>
-        </tr>
+    let roomlist = rooms.map((room) => {
+        const liste = (
+            <tr key={room.Roomnumber} id={room.Roomnumber} onClick={() => { setRoomnumber(room.Roomnumber); let allElements = document.getElementsByClassName('selected'); for (let i = 0; i < allElements.length; i++) { allElements[i].classList.remove('selected'); }; document.getElementById(`${room.Roomnumber}`).classList.add('selected') }} style={{ cursor: 'pointer' }}>
+                <td>{room.Roomnumber}</td>
+                <td>{room.Roomdescritpion}</td>
+                <td>{room.Roomspecials}</td>
+                <td>{room.Roomcapacity}</td>
+            </tr>
+        )
+        if (room.Roomspecials.toString().toLocaleLowerCase().includes(filter.toLocaleLowerCase()) && filterOption === 'Properties') {
+            return liste
+        }
+        else if (room.Roomdescritpion.toLocaleLowerCase().includes(filter.toLocaleLowerCase()) && filterOption === 'Description') {
+            return liste
+        }
+        else if (String(room.Roomnumber).toLocaleLowerCase().includes(filter.toLocaleLowerCase()) && filterOption === 'Roomnumber') {
+            return liste
+        }
+        else if (filterOption === '' || filter === '') {
+            return liste
+        }
+    }
     )
     useEffect(() => {
         setFormattedStartTime(format(startTime, 'yyyy-MM-dd kk:mm'))
@@ -30,6 +50,11 @@ function ReservationPage({ email, reservationPage, setReservationPage,
     }, [startTime, endTime, setFormattedEndTime, setFormattedStartTime])
 
 
+    const options = [
+        { value: 'Roomnumber', label: 'Roomnumber' },
+        { value: 'Description', label: 'Description' },
+        { value: 'Properties', label: 'Properties' },
+    ]
 
 
 
@@ -108,6 +133,10 @@ function ReservationPage({ email, reservationPage, setReservationPage,
                         HomePage
                     </button>
                 </nav>
+                <div className='filterdiv'>
+                    <Select options={options} onChange={(values) => setFilterOption(values.value)} />
+                    <input type="text" placeholder='Enter filter here!' className='filterInput' onChange={(e) => { setFilter(e.target.value) }} />
+                </div>
                 <div className='datetimepickerlabels'>
                     <label>Start:</label>
                     <label className='endlabel'>End:</label>
@@ -123,7 +152,7 @@ function ReservationPage({ email, reservationPage, setReservationPage,
                                 <tr>
                                     <th>Room number</th>
                                     <th>Room description</th>
-                                    <th>Room specials</th>
+                                    <th>Room properties</th>
                                     <th>Room capacity</th>
                                 </tr>
                             </thead>
