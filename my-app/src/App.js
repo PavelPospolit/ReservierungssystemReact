acbbc9324aef5a27d8984fb30a6ae9ec39d229be
PavelPospolit <pavel.pospolit@telekom.de>
2022-03-14 12:49:04 +0100

    removal of useles state declarations

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,17 +1,12 @@
 import StartingPage from './components/StartingPage';
-import React, { useState } from 'react'
+import React from 'react'
 
 
 function App() {
 
-  const [logIn, setLogIn] = useState(true)
-
   return (
     <>
-      <StartingPage
-        logIn={logIn}
-        setLogIn={setLogIn}
-      />
+      <StartingPage />
     </>
   )
 }
