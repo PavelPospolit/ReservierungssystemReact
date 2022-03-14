cb7a06f739e903d706936e587470320c48817f9c
PavelPospolit <pavel.pospolit@telekom.de>
2022-03-14 09:34:35 +0100

    minor changes

--- react-backend/dbFiles/database.js
+++
@@ -1,7 +0,0 @@
-const Sequelize = require('sequelize')
-
-module.exports = new Sequelize('Reservierungssystem', 'Test', 'Test', {
-    host: 'BE1BD320',
-    dialect: 'mssql',
-    operatorAliases: false
-})
